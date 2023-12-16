cb66f850acb8d380bd02ad6a66f0e6b29df19054
AyeshaRafi1 <ayesha.rafi@tintash.com>
2023-12-17 01:11:45 +0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeFeed from './pages/HomeFeed';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeFeed />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomeFeed/index.jsx
@@ -0,0 +1,8 @@
+const HomeFeed = () => {
+  
+  return (
+    <div>This is home feed</div>
+  );
+};
+
+export default HomeFeed;
